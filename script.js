a80eecbb8e3e0fa48c4278e4f561cc24c7433bb4
jabirmahmud24 <jabir.mahmud.saad@gmail.com>
2025-08-26 23:26:09 +0600

    Copy Counter Added

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function(id){
-    const element = document.getElementById(id);
-    return element;
+const getFunc = function (id) {
+  const element = document.getElementById(id);
+  return element;
+};
+// Copy Count
+let copyCount = 0;
+const copyCounter = document.getElementById("copy-counter");
+const copyBtns = document.querySelectorAll(".copy-btn");
 
+for (const btn of copyBtns) {
+  btn.addEventListener("click", function (e) {
+    e.preventDefault(); // Note: this needs parentheses to work properly
+    copyCount++; // Increase total
+    copyCounter.innerText = copyCount;
+  });
 }
