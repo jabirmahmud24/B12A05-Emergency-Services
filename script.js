6f572edaf40015e2421b9690f5b2e8971d728de4
jabirmahmud24 <jabir.mahmud.saad@gmail.com>
2025-08-27 07:15:13 +0600

    Heart Counter Added, Copy Button & Function Modified

--- script.js
+++ script.js
@@ -12,5 +12,33 @@ for (const btn of copyBtns) {
     e.preventDefault(); // Note: this needs parentheses to work properly
     copyCount++; // Increase total
     copyCounter.innerText = copyCount;
+    // ________________________
+    const parentCard = btn.closest(".card");
+    if (!parentCard) return;
+
+    const thirdDiv = parentCard.children[2];
+    if (!thirdDiv || !thirdDiv.firstElementChild) return;
+
+    const textToCopy = thirdDiv.firstElementChild.innerText;
+
+    // Copy to clipboard
+    if (navigator.clipboard && navigator.clipboard.writeText) {
+      navigator.clipboard
+        .writeText(textToCopy)
+        .then(() => alert("Number Coppied: " + textToCopy))
+        .catch((err) => console.error("Failed to copy:", err));
+    }
+  });
+}
+// Heart Count
+let heartCount = 0;
+const heartCounter = document.getElementById("heart-counter");
+const heartBtns = document.querySelectorAll(".heart-btn");
+
+for (const btn of heartBtns) {
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    heartCount++;
+    heartCounter.innerText = heartCount;
   });
 }
